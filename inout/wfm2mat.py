1dcfa015b0edef48871b301e1b6e0cc8e1e17575
Davide Maddaloni <78477737+dmaddalo@users.noreply.github.com>
2023-10-10 18:08:35 +0200

    package inout

--- inout/wfm2mat.py
+++ inout/wfm2mat.py
@@ -3,12 +3,19 @@ import pandas as pd
 
 def f(directory):
     # files = glob.glob(os.path.join(directory,'*.txt'))
-    i = 1
+    i = 0
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
             # with open(os.path.join(directory,filename),'r') as file:
                 # alldata = [line.strip() for line in file.readlines()]
-            tempdata = pd.read_csv(os.path.join(directory,filename),sep='\t',header=24)
+            tempdata = np.array(pd.read_csv(os.path.join \
+                    (directory,filename),sep='\t',header=23))
             # trial = np.array(alldata[24:-1])
-            # WaveData = np.array(tempdata)
+            if i == 0:
+                WaveData = tempdata[...,np.newaxis]
+            else:
+                WaveData = np.dstack((WaveData,tempdata))
+            i += 1
+
+    print('okay')
     # return realization
