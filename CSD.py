8e8910df8950cc6a202fabdbfd4cc5a7f118f277
Davide Maddaloni <78477737+dmaddalo@users.noreply.github.com>
2023-10-20 17:38:18 +0200

    komega performed, check results

---
+++ CSD.py
@@ -0,0 +1,41 @@
+from general import *
+
+
+def f(fs1,fs2,f,*args):
+    if args:
+        flim = args[0]
+    else:
+        flim = f[-1]
+        
+    if len(args) > 1:
+        chunks = args[1]
+    else:
+        chunks = 1
+
+    
+    m = np.argwhere(abs(f-flim) < f[1]-f[0])
+    m = int(m[-1])
+    n = fs1.shape[1]
+    
+    # Cut arrays
+    fs1 = fs1[0:m+1,:]
+    fs2 = fs2[0:m+1,:]
+    f = f[0:m+1]
+    # Allocate
+    csd = np.zeros((m+1,n),dtype=complex)
+    psd1 = np.zeros((m+1,n),dtype=complex)
+    psd2 = np.zeros((m+1,n),dtype=complex)
+    # Assign
+    for i in range(n):
+        csd[:,i] = np.multiply(fs1[:,i],np.conj(fs2[:,i]))
+        psd1[:,i] = np.multiply(fs1[:,i],np.conj(fs1[:,i]))
+        psd2[:,i] = np.multiply(fs2[:,i],np.conj(fs2[:,i]))
+    
+    csd = csd/chunks
+    psd1 = psd1/chunks
+    psd2 = psd2/chunks
+    
+    out = [csd,psd1,psd2,f]
+    
+    return out
+    
---
+++ __init__.py
@@ -0,0 +1,7 @@
+print(f'Loading {__name__} package complete')
+
+__all__ = [
+        'computefft',
+        'CSD',
+        'komega_binning'
+        ]
